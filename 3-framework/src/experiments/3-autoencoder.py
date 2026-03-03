c483a0f76ebd10d9237577ba77efb95d60f66ba9
Vladyslav Libov <vladyslav.libov@gmail.com>
2026-03-03 13:58:40 +0100

    add training of MNIST

--- 3-framework/src/experiments/3-autoencoder.py
+++ 3-framework/src/experiments/3-autoencoder.py
@@ -1,6 +1,6 @@
 import mlflow
 
-from ..utils.datasets import cifar10
+from ..utils.datasets import cifar10, mnist
 from ..utils.metrics import get_accuracy
 from ..models.Autoencoder import Autoencoder 
 from ..training.train_autoencoder import train_autoencoder
@@ -10,30 +10,36 @@ from ..training.train_autoencoder import train_autoencoder
 mlflow.set_tracking_uri(
     "http://localhost:5000"
 )
-mlflow.set_experiment("Autoencoder-CIFAR10")
-# mlflow.enable_system_metrics_logging()
 
+mlflow.set_experiment("Autoencoder-CIFAR10")
 train_loader, val_loader, test_loader = cifar10()
+rgb = True
+
+#mlflow.set_experiment("Autoencoder-MNIST")
+#train_loader, val_loader, test_loader = mnist()
+#rgb = False
+
+# mlflow.enable_system_metrics_logging()
 
 all_params = []
 base_params = {
     "latent_dim": 256,
     "dropout": 0.0,
-    "epochs": 101,
+    "epochs": 21,
     "learning_rate": 1e-3,
     "reduce_lr": "cosine_annealing"
 }
 
 #latent_dim_values = [10, 100, 256] # 10 doesn't really work; 100 and 256 seem to be similar, at least until epoch = 20
-latent_dim_values = [100, 256]
+#latent_dim_values = [100, 256] # 100m vs 256 seems to be very similar, even with 100 epochs
+latent_dim_values = [100]
 for latent_dim in latent_dim_values:
     params = base_params.copy()
     params["latent_dim"] = latent_dim
     all_params.append(params)
 
 for params in all_params:
-    with mlflow.start_run(run_name = f"latent_dim-{params['latent_dim']}-lr-{params['learning_rate']}-dropout-{params['dropout']}-epochs-{params['epochs']}"):
-
+    with mlflow.start_run(run_name = f"latent_dim-{params['latent_dim']}-lr-{params['learning_rate']}-dropout-{params['dropout']}-epochs-{params['epochs']}-reduce_lr-{params['reduce_lr']}"):
         mlflow.log_params(params)
-        autoencoder = Autoencoder(dropout=params["dropout"], latent_dim=params["latent_dim"])
+        autoencoder = Autoencoder(dropout=params["dropout"], latent_dim=params["latent_dim"], rgb=rgb)
         final_model_id = train_autoencoder(autoencoder, train_loader, val_loader, num_epochs=params["epochs"], lr=params["learning_rate"], reduce_lr = params["reduce_lr"])
