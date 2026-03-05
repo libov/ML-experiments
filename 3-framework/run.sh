2b3c698ef8674368e3ff68e83efb9303764c0e4a
Vladyslav Libov <vladyslav.libov@gmail.com>
2026-03-05 18:29:55 +0100

    run config update

--- 3-framework/run.sh
+++ 3-framework/run.sh
@@ -1,4 +1,5 @@
-python -m src.experiments.classifier --experiment_name ResNet-CIFAR10 --dataset cifar10 --dropout 0.0 --epochs 51 --learning_rate 0.001 --reduce_lr cosine_annealing --nruns 1
-python -m src.experiments.classifier --experiment_name ResNet-MNIST   --dataset mnist   --dropout 0.0 --epochs 51 --learning_rate 0.001 --reduce_lr cosine_annealing --nruns 1
+python -m src.experiments.classifier --experiment_name ResNet-CIFAR10 --dataset cifar10 --dropout 0.0 --epochs 101 --optimizer adam --learning_rate 0.001 --reduce_lr cosine_annealing --eta_min 0.0 --nruns 1
+python -m src.experiments.classifier --experiment_name ResNet-CIFAR10 --dataset cifar10 --dropout 0.0 --epochs 301 --optimizer sgd  --learning_rate 0.1   --reduce_lr cosine_annealing --eta_min 0.0 --nruns 1
+python -m src.experiments.classifier --experiment_name ResNet-MNIST   --dataset mnist   --dropout 0.0 --epochs 51  --optimizer adam --learning_rate 0.001 --reduce_lr cosine_annealing --eta_min 0.0 --nruns 1
 #python -m src.experiments.3-autoencoder
 #python -m src.analysis.test-autoencoder
