6a40d3cbe8bac101884bba7bf785ce41b8f56700
Vladyslav Libov <vladyslav.libov@gmail.com>
2026-03-08 11:33:24 +0100

    include test of VAE to top level run script

--- 3-framework/run.sh
+++ 3-framework/run.sh
@@ -11,3 +11,6 @@ python -m src.analysis.test-autoencoder --experiment_name Autoencoder-CIFAR10   
 
 python -m src.experiments.train --task vae  --experiment_name VAE-MNIST     --dataset mnist   --dropout 0.0 --epochs 51  --optimizer adam  --learning_rate 0.001 --lr_scheduler cosine_annealing --eta_min 0.0 --weight_decay 0.0    --nruns 1  --latent_dim 100
 python -m src.experiments.train --task vae  --experiment_name VAE-CIFAR10   --dataset cifar10 --dropout 0.0 --epochs 51  --optimizer adam  --learning_rate 0.001 --lr_scheduler cosine_annealing --eta_min 0.0 --weight_decay 0.0    --nruns 1  --latent_dim 100
+
+python -m src.analysis.test-autoencoder --task vae --experiment_name VAE-MNIST      --run_name mnist-dropout-0.0-optimizer-adam-lr-0.001-run0   --plots_dir plots/vae/mnist
+python -m src.analysis.test-autoencoder --task vae --experiment_name VAE-CIFAR10    --run_name cifar10-dropout-0.0-optimizer-adam-lr-0.001-run0 --plots_dir plots/vae/cifar10
