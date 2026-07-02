9445bb70eef626279355643094b013e12d400ac3
Vladyslav Libov <vladyslav.libov@gmail.com>
2026-07-02 16:53:27 +0200

    remove /2 for generator LR

--- 3-framework/src/experiments/train.py
+++ 3-framework/src/experiments/train.py
@@ -136,7 +136,7 @@ def main():
                 final_model_id = train_gan(model,
                                            train_loader,
                                            num_epochs=args.epochs,
-                                           lr_g=args.learning_rate/2,
+                                           lr_g=args.learning_rate,
                                            lr_d=args.learning_rate,
                                            n_discriminator_steps=5,
                                            optimizer_name=args.optimizer)
